4db5e57f15d04bf4a63a7875cba15d36d2681bc0
USUARIO <USUARIO@DESKTOP-827PK2A>
2022-02-15 20:23:59 -0500

    Clase CSV

--- src/archico/CSVReader.java
+++ src/archico/CSVReader.java
@@ -1,5 +1,26 @@
 package archico;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CSVReader {
+	
+	public static void main(String[] args) throws IOException{
+		
+		File csvFile = new File("D:/Converter/input/Csv_file.csv");
+				BufferedReader br = new BufferedReader(new FileReader(csvFile));
+				String line = "";
+				try {
+					while ((line = br.readLine()) != null) {
+						String[] count = line.split(",");
+						System.out.println(count[0]);
+					}
+				}catch (FileNotFoundException e) {
+					e.printStackTrace();
+				}
+	}
 
 }
